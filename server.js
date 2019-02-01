4082938a2373dcd7b0317fcb8578df01eca30d8b
Amr Galal <amrheshamwahba@aucegypt.edu>
2019-02-01 15:19:52 +0200

    finalized the app

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 var app = express(); //constructor for the express application
-//app.use(express.static('assets'))
+
 hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine','hbs')
 hbs.registerHelper('getYear',()=>{
   return new Date().getFullYear();
 })
+
+app.set('view engine','hbs')
 app.listen(3000,()=> console.log('Server started on port 3000'))
+
+var logRequests = (req,res,next)=>{
+  var timeNow = new Date().toString();
+  var log = `Request received, TIME: ${timeNow} URL: ${req.url} METHOD: ${req.method}\n`;
+  fs.appendFile('server.log',log,(err)=>{
+    if(err)
+    console.log('Unable to append');
+  });
+  next();
+}
+var redirectToMaintenance = (req,res,next)=>{
+  res.render('maintenance.hbs')
+}
+app.use(logRequests)
+app.use(redirectToMaintenance)
+app.use(express.static(__dirname+'/assets'))
 app.get('/',(req,res)=>{
   res.render('home.hbs',{
     pageTitle: 'Home',
